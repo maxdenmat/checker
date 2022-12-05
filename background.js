3eacfc092e31c0fd128ab52ca6864b55a4eb77e7
Max Denmat <max.denmat@modolabs.com>
2022-12-04 20:35:42 -0500

    Add files via upload

--- background.js
+++ background.js
@@ -4,16 +4,16 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
-const extensions = 'https://analyze.modolabs.net/applications'
-const webstore  = 'https://analyze.modolabs.net/'
+const analyzeApps = 'https://analyze.modolabs.net/applications';
+const analyze  = 'https://analyze.modolabs.net/';
 
 // When the user clicks on the extension action
 chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
+  if (tab.url.startsWith(analyzeApps) || tab.url.startsWith(analyze)) {
     // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
     const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
     // Next state will always be the opposite
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON'
+    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
 
     // Set the action badge to the next state
     await chrome.action.setBadgeText({
@@ -22,13 +22,13 @@ chrome.action.onClicked.addListener(async (tab) => {
     });
 
     if (nextState === "ON") {
-      // Insert the CSS file when the user turns the extension on
+      // Run the script to turn select all checkboxes
       await chrome.scripting.executeScript({
-        files: ["script.js"],
+        files: ["script_on.js"],
         target: { tabId: tab.id },
       });
     } else if (nextState === "OFF") {
-      // Remove the CSS file when the user turns the extension off
+      // Run the script to uncheck all checkboxes
       await chrome.scripting.executeScript({
         files: ["script_off.js"],
         target: { tabId: tab.id },
