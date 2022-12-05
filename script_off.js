9b3adae35911b28c1993e8879dbfb5f9a33da7de
Max Denmat <max.denmat@modolabs.com>
2022-12-04 20:11:13 -0500

    Add files via upload

---
+++ script_off.js
@@ -0,0 +1,5 @@
+var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+     for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].type == 'checkbox')
+        checkboxes[i].checked = false;
+     }
