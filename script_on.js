910fede7477eb829215225071117b0b7725c5ab2
Max Denmat <max.denmat@modolabs.com>
2022-12-06 19:54:55 -0500

    Delete script_on.js

--- script_on.js
+++
@@ -1,5 +0,0 @@
-var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-     for (var i = 0; i < checkboxes.length; i++) {
-      if (checkboxes[i].type == 'checkbox')
-        checkboxes[i].checked = true;
-     }
