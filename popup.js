6f7adcd942144516feb646d1ad27f8b0714c560b
Max Denmat <max.denmat@modolabs.com>
2024-01-04 10:42:55 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,14 +3,23 @@ document.addEventListener('DOMContentLoaded', function() {
   var selectNoneButton = document.getElementById('select-none');
 
   selectAllButton.addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(tabs[0].id, {code: 'var checkboxes = document.querySelectorAll("input[type=checkbox]"); checkboxes.forEach(function(checkbox) { checkbox.checked = true; });'});
-    });
+    executeScriptOnActiveTab(setCheckboxesState.bind(null, true));
   });
 
   selectNoneButton.addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(tabs[0].id, {code: 'var checkboxes = document.querySelectorAll("input[type=checkbox]"); checkboxes.forEach(function(checkbox) { checkbox.checked = false; });'});
-    });
+    executeScriptOnActiveTab(setCheckboxesState.bind(null, false));
   });
+
+  function setCheckboxesState(checked) {
+    var checkboxes = document.querySelectorAll('input[type=checkbox]');
+    checkboxes.forEach(function(checkbox) {
+      checkbox.checked = checked;
+    });
+  }
+
+  function executeScriptOnActiveTab(callback) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.executeScript(tabs[0].id, { code: '(' + callback + ')()' });
+    });
+  }
 });
